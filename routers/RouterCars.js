13495ec84fb2b86f90c38b516b2f603fe540a721
DutraGames <gabrieldutraamarante@gmail.com>
2021-08-29 10:13:18 -0300

    img in Cars

--- routers/RouterCars.js
+++ routers/RouterCars.js
@@ -5,18 +5,24 @@ const router = express.Router()
 
 /* Variables Cars */
 let Cars = [{
+  id: 1,
+  img: '../img/Tucson.jpg',
     name: 'Tucson',
     year: '2012',
     km: '110.000',
     price: '52.900'
   },
   {
+    id: 2,
+    img: '../img/Golf Sportline.jpg',
     name: 'Golf Sportline',
     year: '2010',
     km: '0',
     price: '43.900'
   },
   {
+    id: 3,
+    img: '../img/Fit lx 1.4.jpg',
     name: 'Fit lx 1.4',
     year: '2010',
     km: '0',
