e5e0ddea550b404a538dd0a9545feb4b76aecb30
DutraGames <gabrieldutraamarante@gmail.com>
2021-08-29 22:44:50 -0300

    heroku

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const useRouter = require('./Router')
 
-
 const app = express()
+const port = process.env.PORT || 3000
 
 app.use('/', useRouter)
 app.use('/cars', useRouter)
 
-app.listen(3000)
+app.listen(port)
 
 
